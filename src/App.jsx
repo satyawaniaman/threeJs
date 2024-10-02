0927c60689d321c35ff39ba1973fcc7ce4a5c009
aman satyawani <amansatyawani10@gmail.com>
2024-10-02 14:13:06 +0530

    day-1

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return (
+    <Canvas camera={{ position: [0, 3, 8] }}>
+      {/* Lighting */}
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 3, 5]} intensity={0.5} />
+
+      {/* Objects */}
+      <group position={[-2, -2, 0]} scale={[2, 2, 2]} rotation-y={Math.PI / 4}>
+        <mesh position={[-1, 0, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh position={[0, 0, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <mesh position={[1, 0, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="green" />
+        </mesh>
+      </group>
+    </Canvas>
+  );
+}
+
+export default App;
